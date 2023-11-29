e5b8d9f3b103f00367f15875c64b3dd0ca934f82
janikJ <71125912+janik-1@users.noreply.github.com>
2023-11-29 11:58:27 +0100

    Create Item

--- src/main/java/fr/efrei/server/service/ItemService.java
+++ src/main/java/fr/efrei/server/service/ItemService.java
@@ -1,6 +1,20 @@
 package fr.efrei.server.service;
 import org.springframework.stereotype.Service;
+import fr.efrei.server.domain.Item;
+import fr.efrei.server.repository.ItemRepository;
+
+import java.util.List;
 
 @Service
 public class ItemService {
+
+    public final ItemRepository itemRepository;
+
+    public ItemService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+    public List<Item> findAll() {
+        return itemRepository.findAll();
+    }
 }
